577e049a35dce4cd7bdf4357d72e7d8d069c1c6c
tomoshin02 <tomoshin02@gmail.com>
2019-12-10 13:42:37 +0900

    learn reactCreateElement and React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-function App() {
-  return <div><p>thanks,world.</p></div>;
+// function App() {
+//   return <div><p>thanks,world.</p></div>;
+// }
+
+// function App() {
+//   return React.createElement(
+//   "div",
+//   null,
+//   "Hello, World!"
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+       <label htmlFor="bar">bar</label>
+       <input type= "text" onChange={() => {console.log("It was changed.")}} />
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
