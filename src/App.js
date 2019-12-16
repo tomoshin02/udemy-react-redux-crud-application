d92b285286dba80f1959d2feb30ab9f669f55660
tomoshin02 <tomoshin02@gmail.com>
2019-12-16 12:03:38 +0900

    Introduce class component and functonal compo-nent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-// function App() {
-//   return <div><p>thanks,world.</p></div>;
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//        <label htmlFor="bar">bar</label>
+//        <input type= "text" onChange={() => {console.log("It was changed.")}} />
+//     </React.Fragment>
+//     )
+//   }
 // }
 
-// function App() {
-//   return React.createElement(
-//   "div",
-//   null,
-//   "Hello, World!"
-//   );
-// }
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-       <label htmlFor="bar">bar</label>
-       <input type= "text" onChange={() => {console.log("It was changed.")}} />
-    </React.Fragment>
-    )
-  }
+const App =() => {
+  return  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
+
 
 export default App;
