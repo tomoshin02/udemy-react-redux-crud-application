a0267987d2ff153c32a21bb503a0c817fff6a9ff
tomoshin02 <tomoshin02@gmail.com>
2019-12-16 13:00:31 +0900

    create props and defaultProps.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
 
+const App = () => {
+  const profiles = [
+    {name:"Taro", age: 18},
+    {name:"Hanako", age: 34},
+    {name:"new"},
+    {age:58}
 
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//        <label htmlFor="bar">bar</label>
-//        <input type= "text" onChange={() => {console.log("It was changed.")}} />
-//     </React.Fragment>
-//     )
-//   }
-// }
-
-const App =() => {
-  return  <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
+  ]
+  return (
+    <div>
+    {
+      profiles.map((profile, index) => {
+      return (
+      <User name={profile.name} age={profile.age} key={index}/>
+    )
+      }
+    )
+  }
   </div>
+  )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}! and I'm {props.age} years old.</div>
 }
 
-
+User.defaultProps = {
+  name:"Unknown",
+  age:1
+}
 
 export default App;
