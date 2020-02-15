5e98a607b3237c849f8dd5095a545d2b119e83d4
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-02-15 14:03:06 +0900

    Revert "Material ui"

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import { reducer as form } from 'redux-form'
-import events from './events'
-
-
-export default combineReducers({ events, form })
-// export default combineReducers({ foo, bar, baz })
